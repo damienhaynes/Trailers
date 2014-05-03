b351a1c23d3a22a3783b5426fa64254ef7dc5602
Damien Haynes <damien.haynes@gmail.com>
2014-05-03 20:55:14 +1000

    Updated translations from Transifex.

---
+++ Trailers/GUI/GUIDialogTrailers.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using MediaPortal.GUI.Library;
+using MediaPortal.Dialogs;
+using Trailers.Localisation;
+
+namespace Trailers.GUI
+{
+    public class GUIDialogTrailers : GUIDialogMenu
+    {
+        #region Overrides
+
+        public override bool Init()
+        {
+            return Load(GUIGraphicsContext.Skin + @"\Trailers.Selection.Menu.xml");
+        }
+
+        public override int GetID
+        {
+            get
+            {
+                return 11898;
+            }
+        }
+        
+        #endregion
+    }
+}
